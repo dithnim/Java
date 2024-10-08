23765793aa6b0a2e65f460fdaa1aa57c5255634f
dithnim <nimsaradineth63@gmail.com>
2024-10-08 21:55:52 +0530

    heloooooooo

---
+++ Test.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import javax.swing.*;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int nums[] = new int[5];
+
+        System.out.println(Arrays.toString(nums));
+    }
+}
